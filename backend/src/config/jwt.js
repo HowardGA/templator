c7147b7dfcecc719bced45292a3f36ab32151fc7
HowardGA <131924292+HowardGA@users.noreply.github.com>
2025-07-06 18:13:06 -0700

    INIT 16 - Remove cookieDomain configuration from JWT settings and cookie utility functions in order for the cookie to work

--- backend/src/config/jwt.js
+++ backend/src/config/jwt.js
@@ -3,7 +3,6 @@ import {parseDurationToSeconds} from '../utils/time.js';
 export const JWTConfig = {
     jwtAccessSecret: process.env.JWT_ACCESS_SECRET,
     jwtRefreshSecret: process.env.JWT_REFRESH_SECRET,
-    cookieDomain: process.env.NODE_ENV === 'production' ? '.vercel.app' : undefined,
     cookieSecure: true, 
     cookieSameSite:'None', 
     accessTokenExpiresInSeconds: parseDurationToSeconds('15m'),
