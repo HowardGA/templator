380c5bffc5d9b17f35340bc0f7f5ac92f4664807
HowardGA <131924292+HowardGA@users.noreply.github.com>
2025-07-06 17:53:26 -0700

    INIT 15 - Refactor cookie domain configuration for production environment in JWT settings and add cookie-test endpoint

--- backend/src/config/jwt.js
+++ backend/src/config/jwt.js
@@ -3,8 +3,7 @@ import {parseDurationToSeconds} from '../utils/time.js';
 export const JWTConfig = {
     jwtAccessSecret: process.env.JWT_ACCESS_SECRET,
     jwtRefreshSecret: process.env.JWT_REFRESH_SECRET,
-
-    cookieDomain: 'templator-eta.vercel.app',
+    cookieDomain: process.env.NODE_ENV === 'production' ? '.vercel.app' : undefined,
     cookieSecure: true, 
     cookieSameSite:'None', 
     accessTokenExpiresInSeconds: parseDurationToSeconds('15m'),
